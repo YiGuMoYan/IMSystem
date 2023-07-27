7669ae1048fa37dd79c079c97c4186e9c3ddbb1b
YiGuMoYan <3194775246@qq.com>
2023-07-27 10:44:57 +0800

    构建基础Server

--- server.go
+++ server.go
@@ -1,8 +1,50 @@
-package IMSystem
+package main
+
+import (
+	"fmt"
+	"net"
+)
 
 type Server struct {
 	Ip   string
 	Port int
 }
 
-// 创建一个Server接口
+// NewServer 创建一个Server接口
+func NewServer(ip string, port int) *Server {
+	server := &Server{
+		Ip:   ip,
+		Port: port,
+	}
+
+	return server
+}
+
+// Handler 当前链接的业务
+func (this *Server) Handler(conn net.Conn) {
+	fmt.Println("连接建立成功")
+}
+
+// Start 启动服务器
+func (this *Server) Start() {
+	// socket listen
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
+	if err != nil {
+		fmt.Println("net.Listen err:", err)
+		return
+	}
+	defer listener.Close()
+
+	// accept
+	for {
+		// accept
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("listener accept err:", err)
+			continue
+		}
+
+		// go handler
+		go this.Handler(conn)
+	}
+}
